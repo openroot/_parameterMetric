dc6542cb0161aad26892d5b935cd4c753d45dd56
Debaprasad Tapader <dev.openroot@gmail.com>
2024-03-27 21:51:27 +0530

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	use parametermetric\home\entrance as entrance;
 	//new entrance\Boot();
 
-	// Unblock following code to view code in action in front-end.
+	// Unblock following code of sampling to view code in action in front-end or defined in respective.
 
 	// A demonstration of code convention used in this project in generic format.
 	new parametermetric\home\dhop\des\Specimen();
