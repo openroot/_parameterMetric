52513e029f70ee69b93685299a1c58a7c92e6e4d
Debaprasad Tapader <dev.openroot@gmail.com>
2024-03-29 16:44:31 +0530

    update

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 ?>
 
 <?php
-	// TODO: 1. Make A TODO 'section''. Unless, jotting down here.
-	// TODO: 2. Add section-counter feature.
-	// TODO: 3. Add 'section' for code of the project analyzer.
+	// TODO: 1. Add section-counter feature.
+	// TODO: 2. Add 'todo-section'. Till then, jot down here.
+	// TODO: 3. Add 'analyzer-section' for code of the project analysis.
 
 
 	// Unblock following code-blocks to view demonstration of code-in-action in front-end or defined in respective.
