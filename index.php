9cb4624d80de481833ccf78662930dc419460bc0
Debaprasad Tapader <dev.openroot@gmail.com>
2024-03-29 10:07:21 +0530

    update

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 	require_once("parametermetric/home/entrance/platform.php");
 	$platform = new parametermetric\home\entrance\Platform();
-
-	$platform->RequireOnceDirectory("home/dhop");
 ?>
 
 <?php
-	new parametermetric\home\entrance\Specimen();
+	// Unblock following code-blocks to view demonstration of code-in-action in front-end or defined in respective.
 
-	// Unblock following code of sampling to view code in action in front-end or defined in respective.
-
-	// A demonstration of code convention used in this project in generic format.
+	// A demonstration of - code convention used in this project in generic format.
+	$platform->RequireOnceDirectory("home/dhop");
 	new parametermetric\home\dhop\des\Specimen();
+
+	// A demonstration of - parametermetric\home\entrance\Specimen().
+	new parametermetric\home\entrance\Specimen();
 ?>
