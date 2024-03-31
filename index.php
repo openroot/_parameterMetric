b218a71c73f438db31a09a565d71762796b245b0
Debaprasad Tapader <dev.openroot@gmail.com>
2024-04-01 05:27:15 +0530

    update

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 	require_once("parametermetric/home/well/heap.php");
-	$platform = new parametermetric\home\well\heap\Platform();
 ?>
 
 <?php
+	$platform = new parametermetric\home\well\heap\Platform();
 
 
-
-
-
-	// Unblock following code-blocks to view demonstration of code-in-action in front-end or defined in respective.
+	// ------------------------------------------------------------------------------------------------------------
+	// Unblock following if() blocks to view demonstration of code-in-action in front-end or defined in respective.
 
 	// A demonstration of: namespace 'parametermetric\home\well\heap'.
-	echo "<h3>new parametermetric\home\well\heap\Specimen();</h3>";
-	new parametermetric\home\well\heap\Specimen();
+	if ($platform) {
+		echo "<h3>new parametermetric\home\well\heap\Specimen();</h3>";
+		new parametermetric\home\well\heap\Specimen();
+	}
 
 	// A demonstration of: namespace 'parametermetric\home\well\joint'.
 	if ($platform->RequireonceFile("home/well", "joint.php")) {
@@ -25,10 +25,15 @@
 	if ($platform->RequireonceDirectory("home/margosa/now")) {
 		echo "<h3>new parametermetric\home\margosa\now\\flower\Specimen();</h3>";
 		new parametermetric\home\margosa\now\flower\Specimen();
-	}/**/
+	}
+	// ------------------------------------------------------------------------------------------------------------
 
+
+	// ------------------------------------------------------------------------------------------------------------
+	// The task needs to be done, is jot down here for now.
 	
 	// TODO: 1. Add section-counter feature.
 	// TODO: 2. Add 'task-section'. Till then, jot down here.
 	// TODO: 3. Add 'analyze-section' for code of the project analysis.
+	// ------------------------------------------------------------------------------------------------------------
 ?>
