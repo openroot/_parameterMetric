b14255f41d5c340cd175c43df43f6fe350d2744d
Debaprasad Tapader <dev.openroot@gmail.com>
2024-04-01 08:01:28 +0530

    update

--- index.php
+++ index.php
@@ -7,50 +7,65 @@
 ?>
 
 <?php
-	class Index {
+	class Launch {
 		private heap\Platform $platform;
 
-		public function __construct() {
+		public function __construct(?string $blockName = null) {
 			$this->platform = new heap\Platform();
-			if ($this->platform) {
-				$this->Execute();
+
+			if ($blockName != null) {
+				$this->Execute($blockName);
 			}
 		}
 
-		private function Execute() {
-			// ------------------------------------------------------------------------------------------------------------
-			// Unblock following if() blocks to view demonstration of code-in-action in front-end or defined in respective.
-
-			// A demonstration of: namespace 'parametermetric\home\well\heap'.
-			/*if ($this->platform) {
-				echo "<h3>new parametermetric\home\well\heap\Specimen();</h3>";
-				new parametermetric\home\well\heap\Specimen();
-			}*/
-
-			// A demonstration of: namespace 'parametermetric\home\well\joint'.
-			if ($this->platform->RequireonceFile("home/well", "joint.php")) {
-				echo "<h3>new parametermetric\home\well\joint\Specimen();</h3>";
-				new parametermetric\home\well\joint\Specimen();
+		public function Execute(string $blockName) {
+			if ($this->platform) {
+				// Following cases to view to demonstrate of code-in-action in front-end as defined in respective.
+				switch ($blockName) {
+					case "heap":
+						// A demonstration of: namespace 'parametermetric\home\well\heap'.
+						if ($this->platform) {
+							echo "<h3>new parametermetric\home\well\heap\Specimen();</h3>";
+							new parametermetric\home\well\heap\Specimen();
+						}
+						break;
+					case "joint":
+						// A demonstration of: namespace 'parametermetric\home\well\joint'.
+						if ($this->platform->RequireonceFile("home/well", "joint.php")) {
+							echo "<h3>new parametermetric\home\well\joint\Specimen();</h3>";
+							new parametermetric\home\well\joint\Specimen();
+						}
+						break;
+					case "joins":
+						// A demonstration of: namespace 'parametermetric\home\well\joins'.
+						if ($this->platform->RequireonceFile("home/well", "joins.php")) {
+							echo "<h3>new parametermetric\home\well\joins\Specimen();</h3>";
+							new parametermetric\home\well\joins\Specimen();
+						}
+						break;
+					case "margosa":
+						// A demonstration of: code convention used in this project in generic format.
+						if ($this->platform->RequireonceDirectory("home/margosa/now")) {
+							echo "<h3>new parametermetric\home\margosa\\now\\flower\Specimen();</h3>";
+							new parametermetric\home\margosa\now\flower\Specimen();
+						}
+						break;
+					default:
+						echo "This is default case.";
+				}
 			}
-
-			// A demonstration of: namespace 'parametermetric\home\well\joins'.
-			if ($this->platform->RequireonceFile("home/well", "joins.php")) {
-				echo "<h3>new parametermetric\home\well\joins\Specimen();</h3>";
-				new parametermetric\home\well\joins\Specimen();
-			}
-
-			// A demonstration of: code convention used in this project in generic format.
-			/*if ($this->platform->RequireonceDirectory("home/margosa/now")) {
-				echo "<h3>new parametermetric\home\margosa\now\\flower\Specimen();</h3>";
-				new parametermetric\home\margosa\now\flower\Specimen();
-			}*/
-			// ------------------------------------------------------------------------------------------------------------
 		}
 	}
 ?>
 
 <?php
-	new Index();
+	$launch = new Launch("margosa");
+	//$launch->Execute("joint");
+	//$launch->Execute("joins");
+	//$launch->Execute("heap");
+?>
+
+<?php
 	// ------------------------------------------------------------------------------------------------------------
 	// The task needs to be done, is jot down here for now.
 	
