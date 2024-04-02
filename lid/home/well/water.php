56e349f8da4273777df36ce3002b2df8c09a39aa
Debaprasad Tapader <dev.openroot@gmail.com>
2024-04-02 13:39:51 +0530

    update

--- lid/home/well/water.php
+++ lid/home/well/water.php
@@ -70,5 +70,5 @@
 ?>
 
 <?php
-	//echo "<pre>water.php: \"Once boil done, at this point, it's executed and data in memory.\"</pre>";
+	/*echo "<pre>water.php: \"Once boil done, at this point, it's executed and data in memory.\"</pre>";*/
 ?>
