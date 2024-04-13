8c1f78096ed4b55413fadfac5bd121596b95a0d2
Debaprasad Tapader <dev.openroot@gmail.com>
2024-04-13 17:49:54 +0530

    update

--- install/onlineinstallfresh.php
+++ install/onlineinstallfresh.php
@@ -2,5 +2,13 @@
 ?>
 
 <?php
+	$url = "https://github.com/openroot/parametermetric/archive/refs/heads/main.zip";
+	$fileBaseName = basename($url);
 
+	if (file_put_contents($fileBaseName, file_get_contents($url))) {
+		echo "File downloaded successfully"; 
+	}
+	else {
+		echo "File downloading failed.";
+	}
 ?>
